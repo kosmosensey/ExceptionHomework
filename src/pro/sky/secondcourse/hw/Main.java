9053a2f92505013b76697c4ec1285972168e6801
kosmo <kosmosensei@gmail.com>
2023-05-12 20:55:32 +0700

    fix

--- src/pro/sky/secondcourse/hw/Main.java
+++ src/pro/sky/secondcourse/hw/Main.java
@@ -4,6 +4,6 @@ import pro.sky.secondcourse.hw.service.UserService;
 
 public class Main {
     public static void main(String[] args) {
-        UserService.validateLogAndPass("Qwerty_123", "Pass_123", "Pass_123");
+        UserService.createUser("Qwerty_123", "Pass_123", "Pass_123");
     }
 }
--- src/pro/sky/secondcourse/hw/service/UserService.java
+++ src/pro/sky/secondcourse/hw/service/UserService.java
@@ -5,26 +5,27 @@ import pro.sky.secondcourse.hw.exceprion.WrongLoginException;
 import pro.sky.secondcourse.hw.exceprion.WrongPasswordException;
 
 public class UserService {
-
-    public static void validateLogAndPass(String login, String password, String confirmPassword) {
+    public static void createUser(String login, String password, String confirmPassword) {
         try {
-            // Проверка логина
-            if (login == null || !login.matches("[a-zA-Z0-9_]+") || login.length() > 20) {
-                throw new WrongLoginException("Неверный формат логина");
-            }
+            validateLogAndPass(login, password, confirmPassword);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-            // Проверка пароля
-            if (password == null || !password.matches("[a-zA-Z0-9_]+") || password.length() > 20) {
-                throw new WrongPasswordException("Неверный формат пароля");
-            }
-
-            // Проверка совпадения пароля
-            if (!password.equals(confirmPassword)) {
-                throw new MatchPasswordException("Пароль не совпадает");
-            }
-
-        } catch (WrongLoginException | WrongPasswordException | MatchPasswordException e) {
-            System.err.println(e.getMessage());
+    public static void validateLogAndPass(String login, String password, String confirmPassword)
+            throws Exception {
+        // Проверка логина
+        if (login == null || !login.matches("[a-zA-Z0-9_]+") || login.length() > 20) {
+            throw new WrongLoginException("Неверный формат логина");
+        }
+        // Проверка пароля
+        if (password == null || !password.matches("[a-zA-Z0-9_]+") || password.length() > 20) {
+            throw new WrongPasswordException("Неверный формат пароля");
+        }
+        // Проверка совпадения пароля
+        if (!password.equals(confirmPassword)) {
+            throw new MatchPasswordException("Пароль не совпадает");
         }
     }
 }
